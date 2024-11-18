03478665aac0e73c1c03826e8887bc86d46f9257
Daniel <nonprivado@gmail.com>
2024-11-18 17:55:44 +0100

    Creado los Fragments de cambio de menus y boton de Back

---
+++ app/src/main/java/com/example/comanderoapp/EntrantesFragment.kt
@@ -0,0 +1,22 @@
+package com.example.comanderoapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class EntrantesFragment : Fragment() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Infla el layout para este fragmento
+        val rootView = inflater.inflate(R.layout.fragment_entrantes, container, false)
+
+        return rootView
+    }
+}
