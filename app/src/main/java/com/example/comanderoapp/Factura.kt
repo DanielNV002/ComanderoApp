26c660dcea0b1608e40124ce8b0da2e65d193876
victor <victorbejar2005@gmail.com>
2024-12-02 17:07:17 +0100

    tercer merge

--- app/src/main/java/com/example/comanderoapp/Factura.kt
+++ app/src/main/java/com/example/comanderoapp/Factura.kt
@@ -24,7 +24,7 @@ class Factura : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_factura)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
